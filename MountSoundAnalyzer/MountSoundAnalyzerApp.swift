cfa6605f44dad1db0469a6fb30af9f165f185639
mountain <phnompenh0531@gmail.com>
2025-05-13 15:43:25 +0900

    Initial Commit

---
+++ MountSoundAnalyzer/MountSoundAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountSoundAnalyzerApp.swift
+//  MountSoundAnalyzer
+//
+//  Created by ゆ〜ぼ〜 on 2025/05/13.
+//
+
+import SwiftUI
+
+@main
+struct MountSoundAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MountSoundAnalyzerTests/MountSoundAnalyzerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MountSoundAnalyzerTests.swift
+//  MountSoundAnalyzerTests
+//
+//  Created by ゆ〜ぼ〜 on 2025/05/13.
+//
+
+import Testing
+@testable import MountSoundAnalyzer
+
+struct MountSoundAnalyzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
